d1b183422e02444271b6450ba975507a6ad99734
Lee McCuller <Lee.McCuller@gmail.com>
2023-11-28 18:16:19 -0600

    added compare_deep to check if file has changed

--- src/wield/utilities/file_io/__init__.py
+++ src/wield/utilities/file_io/__init__.py
@@ -7,6 +7,8 @@
 # with details inline in source files, comments, and docstrings.
 """
 """
+from collections import abc
+import numpy as np
 from .any_io import (
     load_any,
     write_any,
@@ -49,6 +51,47 @@ def load(fname, ftype=None):
     )
 
 
+def compare_deep(d1, d2):
+    """
+    This function walks down into dictionaries and lists to perform an element-by-element comparison.
+    It is different than just the python == operator on dictionaries because it handles numpy arrays
+    appropriately using np.all
+    """
+    if isinstance(d1, abc.Mapping):
+        if len(d1) != len(d2):
+            return False
+        for k, v in d1.items():
+            try:
+                if not compare_deep(v, d2[k]):
+                    return False
+            except KeyError:
+                return False
+            return True
+    elif isinstance(d1, np.ndarray):
+        if not isinstance(d2, np.ndarray):
+            return False
+        try:
+            return np.all(d1 == d2)
+        except Exception:
+            return False
+    elif isinstance(d1, np.generic):
+        if not isinstance(d2, np.generic):
+            return False
+        try:
+            return d1.item() == d2.item()
+        except Exception:
+            return False
+    elif isinstance(d1, (list, tuple)):
+        if len(d1) != len(d2):
+            return False
+        for idx, v in enumerate(d1):
+            if not compare_deep(v, d2[idx]):
+                return False
+            return True
+    else:
+        return d1 == d2
+
+
 __all__ = [
     "load_any",
     "write_any",
@@ -61,4 +104,5 @@ __all__ = [
     "save",
     "load",
     "load_ls",
+    "compare_deep",
 ]
